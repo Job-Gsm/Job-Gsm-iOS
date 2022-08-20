adc28ba9a057c5101e53e50376f1f2b1bc8a2049
minaje0917 <k050918@naver.com>
2022-08-20 14:57:33 +0900

    :sparkles: add idTextField, textLogo

--- Job-GSM/SignInViewController.swift
+++ Job-GSM/SignInViewController.swift
@@ -29,8 +29,25 @@ class SignInViewController: UIViewController {
         $0.layer.cornerRadius = 10
     }
     
+    let textLogo = UILabel().then {
+        $0.text = "Job-GSM"
+        $0.textColor = .black
+        $0.font = UIFont(name: "Kreon-Regular", size: 40)
+    }
+    
+    lazy var idTextField = UITextField().then{
+        $0.placeholder = "ID"
+//        $0.addTarget(self, action: #selector(), for: .editingChanged)
+    }
+    let idUnderLine = UIView().then {
+        $0.backgroundColor = .black
+    }
+    let idIcon = UIImageView().then {
+        $0.image = UIImage(named: "idIcon.png")
+    }
+    
     private func addView() {
-        [Vector2,background].forEach {
+        [Vector2,background,textLogo,idTextField,idUnderLine,idIcon].forEach {
             view.addSubview($0)
         }
     }
@@ -45,6 +62,25 @@ class SignInViewController: UIViewController {
             $0.centerY.equalToSuperview()
             $0.height.equalTo(574)
             $0.width.equalTo(318)
+        }
+        textLogo.snp.makeConstraints{
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(view.snp.top).offset(236)
+        }
+        idTextField.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(view.snp.top).offset(334)
+            $0.leading.equalToSuperview().offset(99)
+        }
+        idUnderLine.snp.makeConstraints{
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(view.snp.top).offset(368)
+            $0.leading.equalToSuperview().offset(70)
+            $0.height.equalTo(1)
+        }
+        idIcon.snp.makeConstraints{
+            $0.top.equalTo(view.snp.top).offset(334)
+            $0.leading.equalToSuperview().offset(69)
         }
     }
 }
