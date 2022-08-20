f7e6027cba45285b8ace54e0b53376aecab366e4
minaje0917 <k050918@naver.com>
2022-08-20 17:08:35 +0900

    :sparkles: forgotPWVC Setting

--- Job-GSM/ForgotPwViewController.swift
+++ Job-GSM/ForgotPwViewController.swift
@@ -14,6 +14,37 @@ class ForgotPwViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        addView()
+        setLayout()
     }
 
+    let vector2 = UIImageView().then {
+        $0.image = UIImage(named: "Vector2.png")
+    }
+    
+    let background = UIView().then{
+        $0.backgroundColor = .white
+        $0.layer.borderColor = UIColor.black.cgColor
+        $0.layer.borderWidth = 1
+        $0.layer.cornerRadius = 10
+    }
+    
+    private func addView() {
+        [vector2,background].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    private func setLayout() {
+        vector2.snp.makeConstraints{
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(view.snp.bottom).offset(0)
+        }
+        background.snp.makeConstraints{
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+            $0.height.equalTo(404)
+            $0.width.equalTo(318)
+        }
+    }
 }
