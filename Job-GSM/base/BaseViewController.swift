445885ae0f4865ac3a98fedc1925177dbc340616
minaje0917 <k050918@naver.com>
2022-10-27 19:42:45 +0900

    :sparkles: add BaseVC

---
+++ Job-GSM/base/BaseViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BaseViewController.swift
+//  Job-GSM
+//
+//  Created by 선민재 on 2022/10/27.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
